eb85abf6bf0108351647fe4fc95c1a6779bf2cf1
Evelyn Frech <evelynmariafrech@gmail.com>
2022-05-09 11:09:54 -0400

    initial commit. Project structure created

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Project = require('./Cars');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Project };
