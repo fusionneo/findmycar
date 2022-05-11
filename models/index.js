4887e06f6755099ed60bd8377a3d7a5659d31aa4
fusionneo <fusion.neoseeker@gmail.com>
2022-05-11 15:03:07 -0400

    Pushing broken code up so we can all try to fix it. :)

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
 const Cars = require('./Cars');
 
-// User.hasMany(Cars, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(Cars, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE'
+});
 
-// Cars.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
+Cars.belongsTo(User, {
+  foreignKey: 'id'
+});
 
 module.exports = { User, Cars };
